d7686f852d618f75d640daf18ee54be23e1640e5
Mizuki Takayama <miz39t@gmail.com>
2020-09-23 18:13:57 +0900

    仮完成

---
+++ ToDo/AddViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddViewController.swift
+//  ToDo
+//
+//  Created by 高山瑞基 on 2020/09/23.
+//  Copyright © 2020 Takayama Mizuki. All rights reserved.
+//
+
+import UIKit
+
+var toDoList = [String]()
+
+class AddViewController: UIViewController {
+
+    @IBOutlet var toDoTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save() {
+        //配列に追加
+        toDoList.append(toDoTextField.text!)
+        
+        //追加ボタンを押したらテキストを空にする
+        toDoTextField.text = ""
+        
+        //変数の中身をUDに追加
+        UserDefaults.standard.set(toDoList, forKey: "TodoList" )
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    //ストーリーボード上で扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //追加画面で入力した内容を取得する
+        if UserDefaults.standard.object(forKey: "TodoList") != nil {
+            toDoList = UserDefaults.standard.object(forKey: "TodoList") as! [String]
+        }
+    }
+    
+    //UITableView、numberOfRowsInSectionの追加(表示するcell数を決める)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //戻り値の設定(表示するcell数)
+        return toDoList.count
     }
 
+    //UITableView、cellForRowAtの追加(表示するcellの中身を決める)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //変数を作る
+        let TodoCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //変数の中身を作る
+        TodoCell.textLabel!.text = toDoList[indexPath.row]
+       
+        //戻り値の設定（表示する中身)
+        return TodoCell
+    }
+    
+    //スワイプしたセルを削除
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            toDoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+            UserDefaults.standard.removeObject(forKey: "TodoList")
+            UserDefaults.standard.set(toDoList, forKey: "TodoList" )
+        }
+    }
 
 }
 
